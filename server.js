e44101a7bd1eb0a764d2a6ed25549c012baad5a3
Seijno <seijnovannieuwkoop@gmail.com>
2024-06-04 12:20:16 +0200

    startup code

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log('Hier komt je server voor Sprint 12.')
+import express from 'express'
+
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
+
+
+// Stel het poortnummer in waar express naar moet gaan luisteren
+app.set('port', process.env.PORT || 8001)
+
+// Start express op, haal daarbij het zojuist ingestelde poortnummer op
+app.listen(app.get('port'), function() {
+  // Toon een bericht in de console en geef het poortnummer door
+  console.log(`Application started on http://localhost:${app.get('port')}`)
+})
