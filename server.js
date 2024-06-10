d2582c51bedbf1395e80d934e6cfc4e1886dbef9
Seijno <seijnovannieuwkoop@gmail.com>
2024-06-10 12:04:42 +0200

    telefoon nav en assets toegevoegd

--- server.js
+++ server.js
@@ -11,17 +11,40 @@ app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 
 const apiUrl = 'https://fdnd-agency.directus.app/items/',
-categorie = await fetchJson (apiUrl + 'deloitte_categories')
-console.log(categorie)
+categorie = await fetchJson (apiUrl + 'deloitte_categories'),
+auditSub = await fetchJson (apiUrl + 'deloitte_subcategories?filter[subcategory_id]=1'),
+innovationSub = await fetchJson (apiUrl + 'deloitte_subcategories?filter[subcategory_id]=2'),
+consultingSub = await fetchJson (apiUrl + 'deloitte_subcategories?filter[subcategory_id]=3');
 
 app.get('/', function(request, response) {
  
-    fetchJson(categorie)
+    fetchJson(categorie, auditSub, innovationSub, consultingSub)
         response.render('index', {           
             items: categorie.data
-            
+
         })
     });
+
+app.get('/audit', function(request, response) {
+    fetchJson(auditSub)
+    response.render('audit.ejs', {
+        auditItems: auditSub.data
+    });
+  });
+
+app.get('/innovation', function(request, response) {
+    fetchJson(innovationSub)
+    response.render('innovation.ejs', {
+        innovationItems: innovationSub.data
+    });
+  });
+
+app.get('/consulting', function(request, response) {
+    fetchJson(consultingSub)
+    response.render('consulting.ejs', {
+        consultingItems: consultingSub.data
+    });
+  });
 
 // Stel het poortnummer in waar express naar moet gaan luisteren
 app.set('port', process.env.PORT || 8001)
